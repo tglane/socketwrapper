c5b7fc8cfca051c44a7bcd56527658987b66ecdb
timog <timo.glane@gmail.com>
2021-06-18 23:23:35 +0200

    feat/ref:TCP sockets now inherit from base_socket class

--- include/socketwrapper/tcp.hpp
+++ include/socketwrapper/tcp.hpp
@@ -2,6 +2,7 @@
 #define SOCKETWRAPPER_NET_TCP_HPP
 
 #include "span.hpp"
+#include "detail/base_socket.hpp"
 #include "detail/utility.hpp"
 #include "detail/async.hpp"
 #include "detail/message_notifier.hpp"
@@ -20,7 +21,7 @@
 namespace net {
 
 template<ip_version IP_VER>
-class tcp_connection
+class tcp_connection : public detail::base_socket<IP_VER>
 {
 protected:
 
@@ -36,8 +37,12 @@ public:
     tcp_connection& operator=(const tcp_connection&) = delete;
 
     tcp_connection(tcp_connection&& rhs) noexcept
+        : detail::base_socket<IP_VER> {std::move(rhs)}
     {
-        *this = std::move(rhs);
+        m_peer = std::move(rhs.m_peer);
+        m_connection = rhs.m_connection;
+
+        rhs.m_connection = connection_status::closed;
     }
 
     tcp_connection& operator=(tcp_connection&& rhs) noexcept
@@ -45,49 +50,34 @@ public:
         // Provide custom move assginment operator to prevent the moved socket from closing the underlying file descriptor
         if(this != &rhs)
         {
-            m_sockfd = rhs.m_sockfd;
-            m_family = rhs.m_family;
+            detail::base_socket<IP_VER>::operator=(std::move(rhs));
+
             m_peer = std::move(rhs.m_peer);
             m_connection = rhs.m_connection;
 
-            rhs.m_sockfd = -1;
             rhs.m_connection = connection_status::closed;
         }
         return *this;
     }
 
     tcp_connection(const std::string_view conn_addr, const uint16_t port_to)
-        : m_sockfd {::socket(static_cast<uint8_t>(IP_VER), static_cast<uint8_t>(socket_type::stream), 0)}, m_family {IP_VER},
+        : detail::base_socket<IP_VER> {socket_type::stream},
           m_connection {connection_status::closed}
     {
-        detail::init_socket_system();
-
-        if(m_sockfd == -1)
-            throw std::runtime_error {"Failed to created socket."};
-
-        const int reuse = 1;
-        if(::setsockopt(m_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
-            throw std::runtime_error {"Failed to set address reusable."};
-
-#ifdef SO_REUSEPORT
-        if(::setsockopt(m_sockfd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
-            throw std::runtime_error {"Failed to set port reusable."};
-#endif
-
         if(detail::resolve_hostname<IP_VER>(conn_addr, port_to, socket_type::stream, m_peer) != 0)
             throw std::runtime_error {"Failed to resolve hostname."};
 
         if constexpr(IP_VER == ip_version::v4)
         {
             auto& ref = std::get<sockaddr_in>(m_peer);
-            if(auto res = ::connect(m_sockfd, reinterpret_cast<sockaddr*>(&ref), sizeof(sockaddr_in)); res != 0)
+            if(auto res = ::connect(this->m_sockfd, reinterpret_cast<sockaddr*>(&ref), sizeof(sockaddr_in)); res != 0)
                 throw std::runtime_error {"Failed to connect."};
             m_connection = connection_status::connected;
         }
         else if constexpr(IP_VER == ip_version::v6)
         {
             auto& ref = std::get<sockaddr_in6>(m_peer);
-            if(auto res = ::connect(m_sockfd, reinterpret_cast<sockaddr*>(&ref), sizeof(sockaddr_in)); res != 0)
+            if(auto res = ::connect(this->m_sockfd, reinterpret_cast<sockaddr*>(&ref), sizeof(sockaddr_in)); res != 0)
                 throw std::runtime_error {"Failed to connect."};
             m_connection = connection_status::connected;
         }
@@ -95,21 +85,6 @@ public:
         {
             static_assert(IP_VER == ip_version::v4 || IP_VER == ip_version::v6);
         }
-    }
-
-    ~tcp_connection()
-    {
-        if(m_connection != connection_status::closed && m_sockfd > 0)
-        {
-            // TODO only do this when the socket is still in the async context
-            detail::async_context::instance().remove(m_sockfd);
-            ::close(m_sockfd);
-        }
-    }
-
-    int get() const
-    {
-        return m_sockfd;
     }
 
     template<typename T>
@@ -143,7 +118,7 @@ public:
     void async_send(span<T>&& buffer, CALLBACK_TYPE&& callback) const
     {
         detail::async_context::instance().add(
-            m_sockfd,
+            this->m_sockfd,
             detail::async_context::WRITE,
             [this, buffer = std::move(buffer), func = std::forward<CALLBACK_TYPE>(callback)]() {
                 size_t bytes_written = send(std::move(buffer));
@@ -181,11 +156,11 @@ public:
         std::condition_variable cv;
         std::mutex mut;
         std::unique_lock<std::mutex> lock {mut};
-        notifier.add(m_sockfd, &cv);
+        notifier.add(this->m_sockfd, &cv);
 
         // Wait for given delay
         const bool ready = cv.wait_for(lock, delay) == std::cv_status::no_timeout;
-        notifier.remove(m_sockfd);
+        notifier.remove(this->m_sockfd);
 
         if(ready)
             return read(span {buffer});
@@ -197,14 +172,14 @@ public:
     void async_read(span<T>&& buffer, CALLBACK_TYPE&& callback) const
     {
         detail::async_context::instance().add(
-            m_sockfd,
+            this->m_sockfd,
             detail::async_context::READ,
             [this, buffer = std::move(buffer), func = std::forward<CALLBACK_TYPE>(callback)]()
             {
                 // Ok to create new span because its a cheap type containing only a view to the real buffer
                 size_t bytes_read = read(span<T> {buffer});
                 if(bytes_read == 0)
-                    detail::async_context::instance().remove(m_sockfd);
+                    detail::async_context::instance().remove(this->m_sockfd);
                 func(bytes_read);
             }
         );
@@ -215,30 +190,30 @@ protected:
     tcp_connection() = default;
 
     tcp_connection(const int socket_fd, const sockaddr_in& peer_addr)
-        : m_sockfd {socket_fd}, m_family {ip_version::v4}, m_peer {peer_addr}, m_connection {connection_status::connected}
+        : detail::base_socket<IP_VER> {socket_fd, ip_version::v4},
+          m_peer {peer_addr},
+          m_connection {connection_status::connected}
     {
         static_assert(IP_VER == ip_version::v4);
     }
 
     tcp_connection(const int socket_fd, const sockaddr_in6& peer_addr)
-        : m_sockfd {socket_fd}, m_family {ip_version::v6}, m_peer {peer_addr}, m_connection {connection_status::connected}
+        : detail::base_socket<IP_VER> {socket_fd, ip_version::v6},
+          m_peer {peer_addr},
+          m_connection {connection_status::connected}
     {
         static_assert(IP_VER == ip_version::v6);
     }
 
     virtual int read_from_socket(char* const buffer_to, size_t bytes_to_read) const
     {
-        return ::recv(m_sockfd, buffer_to, bytes_to_read, 0);
+        return ::recv(this->m_sockfd, buffer_to, bytes_to_read, 0);
     }
 
     virtual int write_to_socket(const char* buffer_from, size_t bytes_to_write) const
     {
-        return ::send(m_sockfd, buffer_from, bytes_to_write, 0);
+        return ::send(this->m_sockfd, buffer_from, bytes_to_write, 0);
     }
-
-    int m_sockfd;
-
-    ip_version m_family;
 
     std::variant<sockaddr_in, sockaddr_in6> m_peer = {};
 
@@ -255,7 +230,7 @@ using tcp_connection_v6 = tcp_connection<ip_version::v6>;
 
 
 template<ip_version IP_VER>
-class tcp_acceptor
+class tcp_acceptor : public detail::base_socket<IP_VER>
 {
 public:
 
@@ -264,8 +239,9 @@ public:
     tcp_acceptor& operator=(const tcp_acceptor&) = delete;
 
     tcp_acceptor(tcp_acceptor&& rhs) noexcept
+        : detail::base_socket<IP_VER> {std::move(rhs)}
     {
-        *this = std::move(rhs);
+        m_sockaddr = std::move(rhs.m_sockaddr);
     }
 
     tcp_acceptor& operator=(tcp_acceptor&& rhs) noexcept
@@ -273,44 +249,29 @@ public:
         // Provide a custom move assginment operator to prevent the moved object from closing the underlying file descriptor
         if(this != &rhs)
         {
-            m_sockfd = rhs.m_sockfd;
-            m_family = rhs.m_family;
-            m_sockaddr = std::move(rhs.m_sockaddr);
+            detail::base_socket<IP_VER>::operator=(std::move(rhs));
 
-            rhs.m_sockfd = -1;
+            m_sockaddr = std::move(rhs.m_sockaddr);
         }
         return *this;
     }
 
     tcp_acceptor(const std::string_view bind_addr, const uint16_t port, const size_t backlog = 5)
-        : m_sockfd {::socket(static_cast<uint8_t>(IP_VER), static_cast<uint8_t>(socket_type::stream), 0)},
-          m_family {IP_VER}
+        : detail::base_socket<IP_VER> {socket_type::stream}
     {
-        if(m_sockfd == -1)
-            throw std::runtime_error {"Failed to create socket."};
-
-        const int reuse = 1;
-        if(::setsockopt(m_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) < 0)
-            throw std::runtime_error {"Failed to set address resusable."};
-
-#ifdef SO_REUSEPORT
-        if(::setsockopt(m_sockfd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(int)) < 0)
-            throw std::runtime_error {"Failed to set port reusable."};
-#endif
-
         if(detail::resolve_hostname<IP_VER>(bind_addr, port, socket_type::stream, m_sockaddr) != 0)
             throw std::runtime_error {"Failed to resolve hostname."};
 
         if constexpr(IP_VER == ip_version::v4)
         {
             auto& sockaddr_ref = std::get<sockaddr_in>(m_sockaddr);
-            if(auto res = ::bind(m_sockfd, reinterpret_cast<sockaddr*>(&sockaddr_ref), sizeof(sockaddr_in)); res != 0)
+            if(auto res = ::bind(this->m_sockfd, reinterpret_cast<sockaddr*>(&sockaddr_ref), sizeof(sockaddr_in)); res != 0)
                 throw std::runtime_error {"Failed to bind."};
         }
         else if constexpr(IP_VER == ip_version::v6)
         {
             auto& sockaddr_ref = std::get<sockaddr_in6>(m_sockaddr);
-            if(auto res = ::bind(m_sockfd, reinterpret_cast<sockaddr*>(&sockaddr_ref), sizeof(sockaddr_in6)); res != 0)
+            if(auto res = ::bind(this->m_sockfd, reinterpret_cast<sockaddr*>(&sockaddr_ref), sizeof(sockaddr_in6)); res != 0)
                 throw std::runtime_error {"Failed to bind."};
         }
         else
@@ -318,19 +279,8 @@ public:
             static_assert(IP_VER == ip_version::v4 || IP_VER == ip_version::v6);
         }
 
-        if(const auto res = ::listen(m_sockfd, backlog); res != 0)
+        if(const auto res = ::listen(this->m_sockfd, backlog); res != 0)
             throw std::runtime_error {"Failed to initiate listen."};
-    }
-
-    ~tcp_acceptor()
-    {
-        if(m_sockfd > 0)
-            ::close(m_sockfd);
-    }
-
-    int get() const
-    {
-        return m_sockfd;
     }
 
     tcp_connection<IP_VER> accept() const
@@ -339,7 +289,7 @@ public:
         {
             sockaddr_in client {};
             socklen_t len = sizeof(sockaddr_in);
-            if(const int sock = ::accept(m_sockfd, reinterpret_cast<sockaddr*>(&client), &len); sock > 0)
+            if(const int sock = ::accept(this->m_sockfd, reinterpret_cast<sockaddr*>(&client), &len); sock > 0)
                 return tcp_connection<IP_VER> {sock, client};
             else
                 throw std::runtime_error {"Failed to accept."};
@@ -348,7 +298,7 @@ public:
         {
             sockaddr_in6 client {};
             socklen_t len = sizeof(sockaddr_in6);
-            if(const int sock = ::accept(m_sockfd, reinterpret_cast<sockaddr*>(&client), &len); sock > 0)
+            if(const int sock = ::accept(this->m_sockfd, reinterpret_cast<sockaddr*>(&client), &len); sock > 0)
                 return tcp_connection<IP_VER> {sock, client};
             else
                 throw std::runtime_error {"Failed to accept."};
@@ -365,11 +315,11 @@ public:
         std::condition_variable cv;
         std::mutex mut;
         std::unique_lock<std::mutex> lock {mut};
-        notifier.add(m_sockfd, &cv);
+        notifier.add(this->m_sockfd, &cv);
 
         // Wait for given delay
         const bool ready = cv.wait_for(lock, delay) == std::cv_status::no_timeout;
-        notifier.remove(m_sockfd);
+        notifier.remove(this->m_sockfd);
 
         if(ready)
             return std::optional<tcp_connection<IP_VER>> {accept()};
@@ -381,7 +331,7 @@ public:
     void async_accept(CALLBACK_TYPE&& callback) const
     {
         detail::async_context::instance().add(
-            m_sockfd,
+            this->m_sockfd,
             detail::async_context::READ,
             [this, func = std::forward<CALLBACK_TYPE>(callback)]()
             {
@@ -391,10 +341,6 @@ public:
     }
 
 protected:
-
-    int m_sockfd;
-
-    ip_version m_family;
 
     std::variant<sockaddr_in, sockaddr_in6> m_sockaddr {};
 
